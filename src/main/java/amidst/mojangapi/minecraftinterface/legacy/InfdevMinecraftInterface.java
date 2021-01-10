48736120e3f5ff32aa4d8cec9996b515e4196c11
kahomayo <kahomayo@pm.me>
2021-01-11 00:15:42 +0100

    Fix 0618
    Had my instances set up wrongly and didn't get to test it at first.

--- src/main/java/amidst/mojangapi/minecraftinterface/legacy/InfdevMinecraftInterface.java
+++ src/main/java/amidst/mojangapi/minecraftinterface/legacy/InfdevMinecraftInterface.java
@@ -22,7 +22,7 @@ import java.util.stream.IntStream;
 
 public class InfdevMinecraftInterface implements MinecraftInterface {
     public static final RecognisedVersion LAST_COMPATIBLE_VERSION = RecognisedVersion._infdev_0630;
-    public static final InfdevVersion SELECTED_VERSION = InfdevVersion.inf_20100630_1;
+    public static final InfdevVersion SELECTED_VERSION = InfdevVersion.inf_20100618;
     private final RecognisedVersion recognisedVersion;
     private final SymbolicClass chunkGeneratorClass;
     private final SymbolicClass chunkClass;
