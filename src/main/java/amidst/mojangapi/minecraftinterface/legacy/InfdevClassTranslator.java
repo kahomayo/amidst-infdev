dd93bd2867ef84e09802454b61be8dd84bb88a68
kahomayo <kahomayo@pm.me>
2021-01-10 23:27:51 +0100

    Make 0624 work

--- src/main/java/amidst/mojangapi/minecraftinterface/legacy/InfdevClassTranslator.java
+++ src/main/java/amidst/mojangapi/minecraftinterface/legacy/InfdevClassTranslator.java
@@ -1,5 +1,6 @@
 package amidst.mojangapi.minecraftinterface.legacy;
 
+import amidst.clazz.real.AccessFlags;
 import amidst.clazz.translator.ClassTranslator;
 
 public enum InfdevClassTranslator {
@@ -29,6 +30,7 @@ public enum InfdevClassTranslator {
             case inf_20100617_1:
             case inf_20100617_2:
             case inf_20100618:
+                return "h";
             case inf_20100624:
             case inf_20100625_1:
             case inf_20100625_2:
@@ -36,7 +38,37 @@ public enum InfdevClassTranslator {
             case inf_20100629:
             case inf_20100630_1:
             case inf_20100630_2:
-                return "h";
+                return "b";
+        }
+        return "g";
+    }
+
+    private static String getFieldWorldSeed() {
+        switch (InfdevMinecraftInterface.SELECTED_VERSION) {
+            case inf_20100327:
+            case inf_20100330_1:
+            case inf_20100330_2:
+            case inf_20100413:
+            case inf_20100414:
+            case inf_20100415:
+            case inf_20100420:
+            case inf_20100607:
+            case inf_20100608:
+            case inf_20100611:
+            case inf_20100615:
+            case inf_20100616:
+            case inf_20100617_1:
+            case inf_20100617_2:
+            case inf_20100618:
+                return "l";
+            case inf_20100624:
+            case inf_20100625_1:
+            case inf_20100625_2:
+            case inf_20100627:
+            case inf_20100629:
+            case inf_20100630_1:
+            case inf_20100630_2:
+                return "m";
         }
         return "g";
     }
@@ -50,12 +82,16 @@ public enum InfdevClassTranslator {
                     && c.searchForStringContaining("level.dat")
                     && c.searchForStringContaining("SizeOnDisk"))
                 .thenDeclareRequired(InfdevSymbolicNames.CLASS_WORLD)
-                    .requiredField(InfdevSymbolicNames.FIELD_WORLD_SEED, "l")
+                    .requiredField(InfdevSymbolicNames.FIELD_WORLD_SEED, getFieldWorldSeed())
             .next()
-                .ifDetect(c ->
-                    c.searchForStringContaining("xPos")
-                    && c.searchForStringContaining("zPos")
-                    && c.searchForStringContaining("HeightMap"))
+                .ifDetect(c -> {
+                            if (InfdevMinecraftInterface.SELECTED_VERSION.ordinal() < InfdevMinecraftInterface.InfdevVersion.inf_20100624.ordinal())
+                                return c.searchForStringContaining("xPos")
+                                        && c.searchForStringContaining("zPos")
+                                        && c.searchForStringContaining("HeightMap");
+                            else
+                                return c.searchForStringContaining("Attempted to place a tile entity where there was no entity tile!");
+                })
                 .thenDeclareRequired(InfdevSymbolicNames.CLASS_CHUNK)
                    .requiredField(InfdevSymbolicNames.FIELD_CHUNK_BLOCKS, getFieldChunkBlocks())
                    .requiredMethod(InfdevSymbolicNames.METHOD_CHUNK_CALLBACK, "a").end()
